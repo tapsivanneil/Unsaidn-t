1873222da0e90630e2bd3d55c87f8ecf410a037e
Ivan Neil Tapel <tapsivanneil@gmail.com>
2022-12-23 20:58:36 +0800

    Adding the main page

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/homepage.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <title>Unsaidn't</title>
+</head>
+<body class="background1">
+    <div>
+        <section class="section-one">
+            <div class="container-type">
+                <h1 class="webname">unsaidn't</h1>
+            </div>
+            <h5 class="tagline">tell your message anonymously</h5>
+        </section>
+
+        <section class="section-two">
+            <a href="loginsystem/login.php" class="loginButton1">Login</a>
+            <a href="loginsystem/register.php" class="signUpButton">Signup</a>
+        </section>
+    </div>
+
+</body>
+</html>
